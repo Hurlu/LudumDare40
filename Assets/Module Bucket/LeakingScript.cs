8a7fd15bbf926f5bb6e9ec5d486e5123faf690c8
Hugo Willaume <hugo.willaume@epitech.eu>
2017-12-02 18:52:37 +0100

    Bucket module done

---
+++ Assets/Module Bucket/LeakingScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LeakingScript : MonoBehaviour
+{
+
+    public float LeakFrequency;
+    public GameObject WaterDrop;
+    private GameObject _currentDrop = null;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    StartCoroutine(LeakingRoutine());
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+
+	}
+
+    IEnumerator LeakingRoutine()
+    {
+        while (true)
+        {
+            _currentDrop = Instantiate(WaterDrop);
+            _currentDrop.transform.SetParent(transform);
+            _currentDrop.transform.position =
+               new Vector3(_currentDrop.transform.position.x, _currentDrop.transform.position.y, transform.position.z);
+            yield return new WaitForSeconds(LeakFrequency);
+        }
+
+    }
+}
---
+++ Assets/Module Bucket/WaterDropScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterDropScript : MonoBehaviour
+{
+    public float Speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    transform.position = new Vector3(transform.position.x, transform.position.y - Speed, transform.position.z);
+	    if (transform.position.y < -50)
+	        Destroy(this);
+	}
+}
