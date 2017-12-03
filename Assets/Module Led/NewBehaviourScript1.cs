161041890192e0a592e54ed5dd3249e113eeb05a
Tiflo <tiflozion@hotmail.fr>
2017-12-02 16:11:34 -0800

    Manage Shuffle for Lights array

--- Assets/Module Led/NewBehaviourScript1.cs
+++ Assets/Module Led/NewBehaviourScript1.cs
@@ -1,14 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class NewBehaviourScript1 : MonoBehaviour {
     public Light [] array = new Light [6];
 
-    // Use this for initialization
-    void Start () {
-        //l1 = GetComponent<Light>();
+
+    private int RandomNumber(int min, int max)
+    {
+        System.Random random = new System.Random();
+        return random.Next(min, max);
     }
+
+   private void Shuffle<T>(T[] array)
+    {
+        System.Random random = new System.Random();
+        int n = array.Length;
+        for (int i = 0; i < n; i++)
+        {
+            int r = i + random.Next(n - i);
+            T t = array[r];
+            array[r] = array[i];
+            array[i] = t;
+        }
+    }
+
     void InputKeyborad() {
         if (Input.GetKeyUp(KeyCode.Alpha1))
         {
@@ -39,6 +56,16 @@ public class NewBehaviourScript1 : MonoBehaviour {
         {
             array[5].enabled = !array[5].enabled;
         }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            Shuffle(array);
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+
     }
 
     // Update is called once per frame
