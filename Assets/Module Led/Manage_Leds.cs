fae321d25f392c779ebf126d27f01fafabc7ffe7
Tiflo <tiflozion@hotmail.fr>
2017-12-03 09:13:10 -0800

    Add an array for bindings

--- Assets/Module Led/Manage_Leds.cs
+++ Assets/Module Led/Manage_Leds.cs
@@ -5,7 +5,7 @@ using System;
 
 public class Manage_Leds : MonoBehaviour {
     #region "Variables"
-
+    public KeyCode[] Keycode_array = new KeyCode[6];
     public SpriteRenderer [] SR_lamps_array = new SpriteRenderer [6];
     public SpriteRenderer[] SR_number_array = new SpriteRenderer[6];
     public Sprite [] S_on_array = new Sprite [6];
@@ -23,38 +23,38 @@ public class Manage_Leds : MonoBehaviour {
         return random.Next(min, max);
     }
 
-    private void Shuffle(SpriteRenderer[] array, Sprite[] array2, Sprite[] array3, Boolean[] array4, Sprite[] array5, SpriteRenderer[] array6)
+    private void Shuffle()
     {
         System.Random random = new System.Random();
-        int n = array.Length;
+        int n = SR_lamps_array.Length;
         for (int i = 0; i < n; i++)
         {
-            int r = i + random.Next(n - i);
-            SpriteRenderer t = array[r];
-            array[r] = array[i];
-            array[i] = t;
+            int r = RandomNumber(0, n);
 
-            Sprite t2 = array2[r];
-            array2[r] = array2[i];
-            array2[i] = t2;
+            Sprite t2 = S_on_array[r];
+            S_on_array[r] = S_on_array[i];
+            S_on_array[i] = t2;
 
-            Sprite t3 = array3[r];
-            array3[r] = array3[i];
-            array3[i] = t3;
+            Sprite t3 = S_off_array[r];
+            S_off_array[r] = S_off_array[i];
+            S_off_array[i] = t3;
 
-            Boolean t4 = array4[r];
-            array4[r] = array4[i];
-            array4[i] = t4;
+            Boolean t4 = L_is_on[r];
+            L_is_on[r] = L_is_on[i];
+            L_is_on[i] = t4;
 
-            Sprite t5 = array5[r];
-            array5[r] = array5[i];
-            array5[i] = t5;
+            Sprite t5 = S_number_array[r];
+            S_number_array[r] = S_number_array[i];
+            S_number_array[i] = t5;
 
-            SpriteRenderer t6 = array6[r];
-            array6[r] = array6[i];
-            array6[i] = t6;
-            for (int j = 0; j < 6; j++)
-                array6[j].sprite = array5[j];
+            KeyCode t6 = Keycode_array[r];
+            Keycode_array[r] = Keycode_array[i];
+            Keycode_array[i] = t6;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            SR_lamps_array[i].sprite = (L_is_on[i]) ? S_on_array[i] : S_off_array[i];
+            SR_number_array[i].sprite = S_number_array[i];
         }
     }
     
@@ -63,7 +63,7 @@ public class Manage_Leds : MonoBehaviour {
     #region "Keyboard Inputs"
 
     void InputKeyborad() {
-        if (Input.GetKeyUp(KeyCode.Alpha1))
+        if (Input.GetKeyUp(Keycode_array[0]))
         {
             if (L_is_on[0] == false)
             {
@@ -77,7 +77,7 @@ public class Manage_Leds : MonoBehaviour {
             }
         }
 
-        if (Input.GetKeyUp(KeyCode.Alpha2))
+        if (Input.GetKeyUp(Keycode_array[1]))
         {
             if (L_is_on[1] == false)
             {
@@ -91,7 +91,7 @@ public class Manage_Leds : MonoBehaviour {
             }
         }
 
-        if (Input.GetKeyUp(KeyCode.Alpha3))
+        if (Input.GetKeyUp(Keycode_array[2]))
         {
             if (L_is_on[2] == false)
             {
@@ -105,7 +105,7 @@ public class Manage_Leds : MonoBehaviour {
             }
         }
 
-        if (Input.GetKeyUp(KeyCode.Alpha4))
+        if (Input.GetKeyUp(Keycode_array[3]))
         {
             if (L_is_on[3] == false)
             {
@@ -119,7 +119,7 @@ public class Manage_Leds : MonoBehaviour {
             }
         }
 
-        if (Input.GetKeyUp(KeyCode.Alpha5))
+        if (Input.GetKeyUp(Keycode_array[4]))
         {
             if (L_is_on[4] == false)
             {
@@ -133,7 +133,7 @@ public class Manage_Leds : MonoBehaviour {
             }
         }
 
-        if (Input.GetKeyUp(KeyCode.Alpha6))
+        if (Input.GetKeyUp(Keycode_array[5]))
         {
             if (L_is_on[5] == false)
             {
@@ -148,7 +148,7 @@ public class Manage_Leds : MonoBehaviour {
         }
         if (Input.GetKeyUp(KeyCode.A))
         {
-            Shuffle(SR_lamps_array, S_on_array, S_off_array, L_is_on, S_number_array, SR_number_array);
+            Shuffle();
         }
     }
 
