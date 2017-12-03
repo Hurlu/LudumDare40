8a896d467fb2cfc492d99b9e7f43a10c9c1b1f39
aedanc <gabriel.forteville@epitech.eu>
2017-12-03 15:18:27 +0100

    module gegere

---
+++ Assets/Module_gégère/PrinteTimeBeforeLoose.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrinteTimeBeforeLoose : MonoBehaviour {
+
+    public GameObject loosing;
+    public Sprite one;
+    public Sprite two;
+    public Sprite three;
+
+    private SpriteRenderer sprite;
+        // Use this for initialization
+	void Start () {
+        sprite = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        switch (loosing.GetComponent<movingNeedle>().loosing)
+        {
+            case 1:
+                sprite.sprite = one;
+                break;
+            case 2:
+                sprite.sprite = two;
+                break;
+            default:
+                sprite.sprite = three;
+                break;
+        }
+    }
+}
