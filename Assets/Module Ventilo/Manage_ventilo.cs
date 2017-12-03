9172d980afac2dbba84902bca36dad70579f1f8f
Hugo Willaume <hugo.willaume@epitech.eu>
2017-12-03 21:10:01 +0100

    Finished AirTube module

--- Assets/Module Ventilo/Manage_ventilo.cs
+++ Assets/Module Ventilo/Manage_ventilo.cs
@@ -4,24 +4,72 @@ using UnityEngine;
 
 public class Manage_ventilo : MonoBehaviour
 {
-    private bool IsPressed = false;
+    public float pressure_speed;
+    private float unpress_speed;
+    public GameObject NeedlePivot;
+    public SpriteRenderer VaporRender;
+    public SpriteRenderer HandleRender;
+    public Sprite pressureSprite;
+    public Sprite unpressSprite;
+    public Vector3 unpressSpritePos;
+    public Vector3 pressSpritePos;
+
+    private bool releasing = false;
+
     // Use this for initialization
     void Start()
     {
-
+        unpress_speed = pressure_speed * 2;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            IsPressed = true;
-        }
+        Debug.Log(NeedlePivot.transform.eulerAngles);
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space))
         {
-            IsPressed = false;
+            Debug.Log("Release");
+            ReleasePressure();
+        }
+        else
+        {
+            Debug.Log("Accu");
+            AccumulatePressure();
+        }
+    }
+
+    void ReleasePressure()
+    {
+        if (!releasing)
+        {
+            releasing = true;
+            HandleRender.sprite = unpressSprite;
+            HandleRender.transform.position = unpressSpritePos;
+            VaporRender.enabled = true;
+        }
+        if (NeedlePivot.transform.localEulerAngles.z < 122 || NeedlePivot.transform.localEulerAngles.z > 240)
+        {
+            NeedlePivot.transform.localEulerAngles =
+                new Vector3(NeedlePivot.transform.localEulerAngles.x, NeedlePivot.transform.localEulerAngles.y,
+                    NeedlePivot.transform.localEulerAngles.z + unpress_speed);
+        }
+    }
+
+    void AccumulatePressure()
+    {
+        if (releasing)
+        {
+            releasing = false;
+            HandleRender.sprite = pressureSprite;
+            HandleRender.transform.position = pressSpritePos;
+            VaporRender.enabled = false;
+        }
+        if (NeedlePivot.transform.localEulerAngles.z < 240 || NeedlePivot.transform.localEulerAngles.z > 250)
+        {
+            NeedlePivot.transform.localEulerAngles =
+                new Vector3(NeedlePivot.transform.localEulerAngles.x, NeedlePivot.transform.localEulerAngles.y,
+                    NeedlePivot.transform.localEulerAngles.z - pressure_speed);
         }
     }
 }
